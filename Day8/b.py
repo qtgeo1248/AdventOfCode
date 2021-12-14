fba830ae8242918c70b848a70d8cc8526029893d
George Zhou <GZhou7819@gmail.com>
2021-12-14 04:22:10 -0500

    Nearly done, jus tneed to figure out deducing what is what

--- Day8/b.py
+++ Day8/b.py
@@ -1,6 +1,7 @@
 import pprint
 
-numPos = 7;
+numPos = 7
+numDigits = 10
 
 # The ones with only one possible length, so can make definite claims
 easy = [
@@ -10,10 +11,23 @@ easy = [
 
 # If you have a possibility on the left, you can't have a length on the right
 hard = {
-    1: 3,
     3: 2,
     4: 3,
     5: 3}
+
+# Decoding
+decoders = [
+    {0, 1, 2, 4, 5, 6},
+    {2, 5},
+    {0, 2, 3, 4, 6},
+    {0, 2, 3, 5, 6},
+    {1, 2, 3, 5},
+    {0, 1, 3, 5, 6},
+    {0, 1, 3, 4, 5, 6},
+    {0, 2, 5},
+    {0, 1, 2, 3, 4, 5, 6},
+    {0, 1, 2, 3, 5, 6}
+]
 
 '''
 My Positions 
@@ -42,40 +56,65 @@ def rem_easy(wires, posses, pos, tester):
 
 # Each thing only has pairs left, so deduces it one by one
 def rem_hard(wires, posses):
+    pp = pprint.PrettyPrinter()
     for i in range(numPos):
-        if len(posses[i]) > 1:
-            notRemoved = True
-            k = 0
-            while notRemoved and k < len(posses[i]):
-                tester = list(posses[i])[k]
-                if tester in hard.keys() and hard[tester] in wires[i]:
-                    notRemoved = False
-                    posses[i].remove(tester)
-                    other = list(posses[i])[0]
-                    for j in range(numPos):
-                        if i != j and other in posses[j]:
-                            posses[j].remove(other)
-                k += 1
+        notRemoved = True
+        pp.pprint(posses)
+        k = 0
+        while notRemoved and k < len(posses[i]):
+            tester = list(posses[i])[k]
+            pp.pprint(tester)
+            if tester in hard.keys() and hard[tester] in wires[i]:
+                notRemoved = False
+                posses[i].remove(tester)
+                other = list(posses[i])[0]
+                for j in range(numPos):
+                    if i != j and other in posses[j]:
+                        posses[j].remove(other)
+            k += 1
+
+def digit(positions):
+    for i in range(numDigits):
+        if positions == decoders[i]:
+            return i
 
 def main():
     f = open("test1.txt")
     pp = pprint.PrettyPrinter()
+    ans = 0
     for line in f:
         wires = init(False)
         code = line.split(" ")
-        i = 0
-        while code[i] != "|":
-            tok = code[i]
+        tokidx = 0
+        while code[tokidx] != "|":
+            tok = code[tokidx]
             for j in range(len(tok)):
                 wires[ord(tok[j]) - ord('a')].add(len(tok) - 2)
-            i += 1
-        
+            tokidx += 1
+        tokidx += 1
+
+        pp.pprint(wires)
         posses = init(True)
         for i in range(numPos):
             for j in range(3):
                 rem_easy(wires, posses, i, j)
         rem_hard(wires, posses)
         pp.pprint(posses)
+
+        rewiring = []
+        for i in range(numPos):
+            rewiring.append(list(posses[i])[0])
+        pp.pprint(rewiring)
+
+        output = 0
+        while tokidx < len(code):
+            tok = code[tokidx].rstrip()
+            positions = set()
+            for i in range(len(tok)):
+                positions.add(rewiring[ord(tok[i]) - ord('a')])
+            output = 10 * output + digit(positions)
+            tokidx += 1
+        pp.pprint(output)
 
 if __name__ == "__main__":
     main()

6c5fd326b5778d57b045609cb1dd0f45781dddf2
George Zhou <GZhou7819@gmail.com>
2022-12-05 00:18:03 -0500

    finished day 5 of 2022

---
+++ 2022/Day5/b.py
@@ -0,0 +1,36 @@
+import pprint
+
+pp = pprint.PrettyPrinter()
+
+def main():
+    f = open("procedure.txt")
+    stacks = []
+    tot = 0
+    for line in f:
+        if line[0] == "m":
+            line = line.strip().split()
+            numMove = int(line[1])
+            fro = int(line[3])
+            to = int(line[5])
+            stacks[to - 1].extend(stacks[fro - 1][-numMove:])
+            stacks[fro - 1] = stacks[fro - 1][0:-numMove]
+
+        elif line[0] == "\n":
+            for i in range(tot):
+                stacks[i].pop()
+                stacks[i].reverse()
+        else:
+            if len(stacks) == 0:
+                tot = len(line) // 4
+                stacks = [[] for _ in range(tot)]
+            for i in range(tot):
+                c = line[4 * i + 1]
+                if c != " ":
+                    stacks[i].append(c)
+    ans = ""
+    for stack in stacks:
+        ans += stack[-1]
+    print(ans)
+
+if __name__ == "__main__":
+    main()
